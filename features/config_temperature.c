458090c41a014f68484d42d6110adf5f8b014184
x86_64 <x86mail@gmail.com>
2012-08-23 15:02:28 +0400

    Remove sensors for a while

--- features/config_temperature.c
+++ features/config_temperature.c
@@ -1,7 +1,7 @@
 #include "common.h"
 
 const temp_sensor_t           temp_sensors[] = {
-	{ 0, 'T', &max6675_init, &max6675_read, (sensor_max6675_userdata []){ { } } }
+	//{ 0, 'T', &max6675_init, &max6675_read, (sensor_max6675_userdata []){ { } } }
 	
 };
 const uint8_t                 temp_sensors_count = (sizeof(temp_sensors) / sizeof(temp_sensors[0]));
